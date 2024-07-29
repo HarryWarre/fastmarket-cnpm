9d6ac5946bd8695bae30981947e7b6f0b3cd8f78
hoangviet111203 <hoangviet111203@gmail.com>
2024-07-30 03:15:49 +0700

    Update dbcontext

--- Entities/FastdbContext.cs
+++ Entities/FastdbContext.cs
@@ -40,8 +40,13 @@ public partial class FastdbContext : DbContext
     public virtual DbSet<TypePayment> TypePayments { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=fastdb;Integrated Security=True;TrustServerCertificate=True");
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            // This part is optional if you are using DI
+            optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=fastdb;Integrated Security=True;TrustServerCertificate=True");
+        }
+    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
