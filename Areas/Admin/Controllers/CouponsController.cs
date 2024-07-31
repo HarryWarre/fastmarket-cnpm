39abc9f031da972160b22a6845054600747a533a
hoangviet111203 <hoangviet111203@gmail.com>
2024-07-31 11:01:18 +0700

    Update Manage Coupons

---
+++ Areas/Admin/Controllers/CouponsController.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using fastwebsite.Entities;
+
+namespace fastwebsite.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CouponsController : Controller
+    {
+        private readonly FastdbContext _context;
+
+        public CouponsController(FastdbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/Coupons
+        public async Task<IActionResult> Index()
+        {
+              return _context.Coupons != null ? 
+                          View(await _context.Coupons.ToListAsync()) :
+                          Problem("Entity set 'FastdbContext.Coupons'  is null.");
+        }
+
+        // GET: Admin/Coupons/Details/5
+        // GET: Admin/Coupons/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Coupons/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Coupon coupon)
+        {
+            if (true)
+            {
+                _context.Add(coupon);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(coupon);
+        }
+
+        // GET: Admin/Coupons/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Coupons == null)
+            {
+                return NotFound();
+            }
+
+            var coupon = await _context.Coupons.FindAsync(id);
+            if (coupon == null)
+            {
+                return NotFound();
+            }
+            return View(coupon);
+        }
+
+        // POST: Admin/Coupons/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Coupon coupon)
+        {
+            if (id != coupon.CouponId)
+            {
+                return NotFound();
+            }
+
+            if (true)
+            {
+                try
+                {
+                    _context.Update(coupon);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CouponExists(coupon.CouponId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(coupon);
+        }
+
+        // GET: Admin/Coupons/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Coupons == null)
+            {
+                return NotFound();
+            }
+
+            var coupon = await _context.Coupons
+                .FirstOrDefaultAsync(m => m.CouponId == id);
+            if (coupon == null)
+            {
+                return NotFound();
+            }
+
+            return View(coupon);
+        }
+
+        // POST: Admin/Coupons/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Coupons == null)
+            {
+                return Problem("Entity set 'FastdbContext.Coupons'  is null.");
+            }
+            var coupon = await _context.Coupons.FindAsync(id);
+            if (coupon != null)
+            {
+                _context.Coupons.Remove(coupon);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CouponExists(int id)
+        {
+          return (_context.Coupons?.Any(e => e.CouponId == id)).GetValueOrDefault();
+        }
+    }
+}
